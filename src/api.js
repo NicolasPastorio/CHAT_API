df7879141725238f90529c8ec398d4f43700db6f
Nicolas Pastorio <npastoriodemoura@gmail.com>
2024-04-02 19:27:12 -0300

    Base da API

--- src/api.js
+++ src/api.js
@@ -5,8 +5,18 @@ app.use(express.json());
 
 const router = express.Router();
 
+//rota raíz
 app.use('/', router.get('/', (req, res) => {
     res.status(200).send("<h1>API - CHAT</h1>")
 }))
+
+//rota sobre
+app.use("/", router.get("/sobre", (req, res, next) =>{
+    res.status(200).send({
+        "nome": "API - CHAT",
+        "versão": "0.1.0",
+        "autor": "Nícolas Pastório"
+    })
+}));
 
 module.exports = app;
